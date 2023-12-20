c31fab85e85327b5e8f48355f96187497f104a80
sagarbhadke08 <sagarbhadke08@gmail.com>
2023-12-20 15:59:12 +0530

    added setimeout

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -16,8 +16,8 @@ function Modal({ open, children, onClose }) {
 
 
   return createPortal(
-    <dialog className="modal" ref={dialog}  onClose={onClose}>
-      {children}
+    <dialog className="modal" ref={dialog} onClose={onClose}>
+      {(open) ? children : null}
     </dialog>,
     document.getElementById('modal')
   );
